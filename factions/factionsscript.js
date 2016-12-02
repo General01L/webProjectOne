cda344c47633fbcde7233e1d14e3d943f3e01706
General01L <l.h.h.kulter@student.tue.nl>
2016-12-02 17:18:07 +0100

    Update factionsscript.js

--- factions/factionsscript.js
+++ factions/factionsscript.js
@@ -1,1 +1,10 @@
+function main() {
+  $('.row-moreinfo').hide();
+  $('.moreinfo-button').on('click', function() {;
+  //$(this).next().toggle();
+  $(this).next().slideToggle(400);
+  $(this).toggleClass('active');
+ });
+}
 
+$(document).ready(main);
