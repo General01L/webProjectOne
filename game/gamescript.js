827f1dc7bd4c577f8dd0b23502b1c5e328b8998e
General01L <l.h.h.kulter@student.tue.nl>
2016-12-02 23:09:50 +0100

    Update gamescript.js

--- game/gamescript.js
+++ game/gamescript.js
@@ -1,6 +1,9 @@
 function main() {
-  //$('.container-choosefaction').hide();
-
+  $('.container-choosefaction').hide();
+  $('.btn btn primary').on('click', function() {;
+  $('.jumbotron-start').fadeOut(800);
+  $('.container-choosefaction').fadeIn(800);
+ });
 }
 
 $(document).ready(main);
