6d2827e39152925e3fa6812f8b769bb1ad7628ae
General01L <l.h.h.kulter@student.tue.nl>
2016-12-02 17:29:56 +0100

    Update factionsscript.js

--- factions/factionsscript.js
+++ factions/factionsscript.js
@@ -1,9 +1,7 @@
 function main() {
   $('.row-moreinfo').hide();
   $('.btn btn-primary').on('click', function() {;
-  //$(this).next().toggle();
-  $(this).next().slideToggle(400);
-  $(this).toggleClass('active');
+  $('.row-moreinfo').next().slideToggle(400);
  });
 }
 
