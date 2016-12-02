851ed5aa981f1b545c5501663ec18f677884d508
General01L <l.h.h.kulter@student.tue.nl>
2016-12-02 18:03:17 +0100

    Update factionsscript.js

--- factions/factionsscript.js
+++ factions/factionsscript.js
@@ -1,7 +1,22 @@
 function main() {
-  //$('.row-moreinfo').hide();
-  $('.moreinfo-button').on('click', function() {;
-  $(this).next('.row-moreinfo').slideToggle(400);
+  $('.row-moreinfoorks').hide();
+  $('.row-moreinfochaos').hide();
+  $('.row-moreinfomarines').hide();
+  $('.row-moreinfosisters').hide();
+  $('.moreinfoorks-button').on('click', function() {;
+  $('.row-moreinfoorks').slideToggle(400);
+  $(this).toggleClass('active');
+ });
+  $('.moreinfochaos-button').on('click', function() {;
+  $('.row-moreinfochaos').slideToggle(400);
+  $(this).toggleClass('active');
+ });
+  $('.moreinfomarines-button').on('click', function() {;
+  $('.row-moreinfomarines').slideToggle(400);
+  $(this).toggleClass('active');
+ });
+  $('.moreinfosisters-button').on('click', function() {;
+  $('.row-moreinfosisters').slideToggle(400);
   $(this).toggleClass('active');
  });
 }
