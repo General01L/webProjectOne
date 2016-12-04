60e023a2171de5b4d253a8475a0e4a77ebc17e2b
General01L <l.h.h.kulter@student.tue.nl>
2016-12-05 00:41:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function adaptcsssize(width) {
+  width = parseInt(width);
+  if (width < 800) {
+    $("#size-stylesheet").attr("href", "mainsmall.css");
+  } else if (width < 2000) {
+    $("#size-stylesheet").attr("href", "css/main.css");
+  } else {
+     $("#size-stylesheet").attr("href", "css/mainlarge.css"); 
+  }
+}
 
+$(function() {
+  adjustStyle($(this).width());
+  $(window).resize(function() {
+    adjustStyle($(this).width());
+  });
+});
